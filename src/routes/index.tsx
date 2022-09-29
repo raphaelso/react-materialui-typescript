78ed529c1b37b08675ee91d953b88d45258deb64
Raphael Oliveira <raphaelsoliveira@gmail.com>
2022-09-29 07:15:06 +0100

    feat: adicionado responsividade do menu lateral

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
 import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { useAppThemeContext } from '../shared/contexts';
+import { useAppThemeContext, useDrawerContext } from '../shared/contexts';
 
 
 export const AppRoutes = () => {
   const { toggleTheme } = useAppThemeContext();
+  const { toggleDrawerOpen } = useDrawerContext();
+
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={ <Button variant='contained' color='primary' onClick={toggleTheme}>Trocar Tema</Button> } />
+      <Route path="/pagina-inicial" element={ <><Button variant='contained' color='primary' onClick={toggleTheme}>Trocar Tema</Button> <Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Menu</Button></> } />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,46 @@
+import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
+import { Box } from '@mui/system';
+import { useDrawerContext } from '../../contexts';
+
+interface IMenuLateralProps {
+    children: React.ReactNode
+}
+
+export const MenuLateral: React.FC<IMenuLateralProps> = ({children}) => {
+  const theme = useTheme();  
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
+
+  return (
+    <>
+      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
+        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+          <Box width='100%' height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              src="https://i.im.ge/2022/09/28/1AzCJF.andalf-clipart-cartoon-gandalf-fireworks-clip-art-11563489517ls9klfjnei.png" />
+          </Box>
+
+          <Divider />
+
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                  <ListItemText primary="Página inicial" />
+                </ListItemIcon>
+              </ListItemButton>
+            </List>
+          </Box>
+
+        </Box>
+      </Drawer>
+      <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  
+  );
+};
